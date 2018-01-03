890dd2fdf541fe47d41e6d983a82ddd0ea9de8a5
mickequeen <mickequeen@gmail.com>
2018-01-03 18:23:02 -0300

    rescatando texto de textarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
-    
-    /*$("[data-toggle=tooltip]").tooltip();*/
+  $('#share').click(function(){
+    var comentario= $('#comment').val();
+    $('#comment').val("");
+    $("#nuevaPub").append("<div class='contComment'>" + comentario + "</div>");
+
+
+
+  })
+  /*$("[data-toggle=tooltip]").tooltip();*/
 
     
 });
